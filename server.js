0828c13f56273441a8a054caf78e4ac0f4c15c24
Luizfdos <luizfernando.os@outlook.com>
2020-05-28 17:01:20 -0300

    Deixado opção de adicionar nova ideia funcional

--- server.js
+++ server.js
@@ -2,55 +2,13 @@
 const express = require("express")
 const server = express()
 
-
-
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em família",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pintura",
-        category: "Criatividade",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category: "Criatividade",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis modi veritatis sapiente quas rerum magni nostrum labore recusandae consequuntur quibusdam unde animi sequi, iure ullam. Saepe, earum debitis",
-        url: "https://rocketseat.com.br"
-    },
-]
+const db = require("./db")
 
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+// habilitar uso do req.body
+server.use(express.urlencoded({extended: true}))
 
 
 // configuração do nunjucks
@@ -65,24 +23,67 @@ nunjucks.configure("views", {
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
+     db.all(`SELECT * FROM ideas`, function(err, row){
+        if (err) return console.log(err)
 
-    let lastIdeas = []
-    for (let idea of reversedIdeas) {
-        if(lastIdeas.length< 2) {
-            lastIdeas.push(idea)
+        const reversedIdeas = [...row].reverse()
+
+        let lastIdeas = []
+        for (let idea of reversedIdeas) {
+            if(lastIdeas.length< 2) {
+                lastIdeas.push(idea)
+            }
         }
-    }
-    
-    return res.render("index.html", { ideas: lastIdeas })
+        
+        return res.render("index.html", { ideas: lastIdeas })
+     })
+
+
 })
 
 server.get("/ideias", function(req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
+        }
 
-    return res.render("ideias.html", { ideas: reversedIdeas })
+        const reversedIdeas = [...rows].reverse()
+
+        return res.render("ideias.html", { ideas: reversedIdeas })
+    })
+
 })
+
+server.post("/", function(req, res){
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+    const values =  [
+       req.body.image,
+       req.body.title,
+       req.body.category,
+       req.body.description,
+       req.body.link,
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
+        }
+
+       return res.redirect("/ideias")
+    })
+})
+
 
 // liguei meu servidor na porta 3000
 server.listen(3000)
